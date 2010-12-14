3d14e40ffdf684a1b8d813fdbd34e84ba6af6e93
Erik Hollensbe <erik@hollensbe.org>
2010-12-13 21:49:23 -0500

    * primary key support.

--- lib/rdbi/driver/postgresql.rb
+++ lib/rdbi/driver/postgresql.rb
@@ -82,7 +82,15 @@ class RDBI::Driver::PostgreSQL < RDBI::Driver
         sch.type = :view
       end
 
-      execute( "SELECT column_name, data_type, is_nullable FROM information_schema.columns WHERE table_schema = ? AND table_name = ?",
+      execute( %q[
+          SELECT c.column_name, c.data_type, c.is_nullable, tc.constraint_type
+          FROM information_schema.columns c
+            LEFT JOIN information_schema.key_column_usage kcu
+              ON kcu.column_name = c.column_name
+              LEFT JOIN information_schema.table_constraints tc
+                ON tc.constraint_name = kcu.constraint_name
+          WHERE c.table_schema = ? and c.table_name = ?;
+        ],
         pg_schema,
         table_name
       ).fetch( :all ).each do |row|
@@ -92,6 +100,7 @@ class RDBI::Driver::PostgreSQL < RDBI::Driver
         # TODO: ensure this ruby_type is solid, especially re: dates and times
         col.ruby_type  = row[1].to_sym
         col.nullable   = row[2] == "YES"
+        col.primary_key = row[3] == "PRIMARY KEY"
         sch.columns << col
       end
 
@@ -100,7 +109,11 @@ class RDBI::Driver::PostgreSQL < RDBI::Driver
 
     def schema( pg_schema = 'public' )
       schemata = []
-      execute( "SELECT table_name FROM information_schema.tables WHERE table_schema = '#{pg_schema}';" ).fetch( :all ).each do |row|
+      execute(%Q[
+        SELECT table_name 
+        FROM information_schema.tables 
+        WHERE table_schema = '#{pg_schema}'
+      ]).fetch( :all ).each do |row|
         schemata << table_schema( row[0], pg_schema )
       end
       schemata
--- test/test_database.rb
+++ test/test_database.rb
@@ -175,6 +175,10 @@ class TestDatabase < Test::Unit::TestCase
     assert_kind_of( RDBI::Schema, res.schema )
     assert res.schema.columns
     res.schema.columns.each { |x| assert_kind_of(RDBI::Column, x) }
+
+    assert(dbh.table_schema('ordinals').columns.find {|x| x.name == :id }.primary_key)
+    assert(!dbh.table_schema('ordinals').columns.find {|x| x.name == :cardinal }.primary_key)
+    assert(!dbh.table_schema('ordinals').columns.find {|x| x.name == :s }.primary_key)
   end
 
   def test_08_datetime
